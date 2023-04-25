6046259fae95b31a91e8e912b11def4cbc331f99
Titus Moore <tmoore@edgewebware.com>
2023-04-25 11:33:46 -0400

    Added dart

--- init.lua
+++ init.lua
@@ -266,7 +266,7 @@ vim.keymap.set('i', '<C-q>', 'copilot#Accept("<CR>")', { expr = true, silent = t
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'php' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'php', 'dart' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
