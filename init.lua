c8306cb467aa936ea9a41e6969e78e84c88d5968
Titus Moore <tmoore@edgewebware.com>
2023-05-05 11:47:26 -0400

    Added C# support in treesitter

--- init.lua
+++ init.lua
@@ -279,7 +279,8 @@ vim.keymap.set('n', '<leader>hp', function() require('harpoon.ui').nav_prev() en
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'php', 'dart' },
+  ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'php',
+    'dart' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
